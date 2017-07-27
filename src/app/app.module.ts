4f31ab7a7e67605f1f6e0ecfe2ae8841d1d4de8b
David Stulajter <davidstulajter@gmail.com>
2017-07-26 19:07:34 -0500

    add second, third todo component, generate service todo, but not implemented yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
